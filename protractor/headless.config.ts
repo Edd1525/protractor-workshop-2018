5e64e94fd8834489ac0944ce79ca2bf4750f4e95
Edward Bustamante Acevedo <Edd1525@users.noreply.github.com>
2018-10-30 07:56:39 -0500

    Changes times (#10)
    * tslint setup
    
    * debug setup
    
    * css locator
    
    * Page Object Model
    
    * changes times

--- protractor/headless.config.ts
+++ protractor/headless.config.ts
@@ -3,10 +3,12 @@ import { Config, browser } from 'protractor';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/google.spec.js' ],
+  specs: ['../test/**/*.spec.js'],
+  getPageTimeout: 30000,
   SELENIUM_PROMISE_MANAGER: false,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(3000);
     reporter();
   },
   capabilities: {
@@ -14,5 +16,8 @@ export const config: Config = {
     chromeOptions: {
       args: ['--headless', '--disable-gpu']
     }
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
   }
 };
---
+++ test/buy-tshirt.spec.ts
@@ -0,0 +1,43 @@
+import { browser } from 'protractor';
+import {
+  AddressStepPage,
+  BankPaymentPage,
+  MenuContentPage,
+  OrderSummaryPage,
+  PaymentStepPage,
+  ProductAddedPage,
+  ProductListPage,
+  ShippingStepPage,
+  SignInStepPage,
+  SummaryStepPage
+} from '../src/page';
+
+describe('Buy a t-shirt', () => {
+  const addressStepPage: AddressStepPage = new AddressStepPage();
+  const bankPaymentPage: BankPaymentPage = new BankPaymentPage();
+  const menuContentPage: MenuContentPage = new MenuContentPage();
+  const orderSummaryPage: OrderSummaryPage = new OrderSummaryPage();
+  const paymentStepPage: PaymentStepPage = new PaymentStepPage();
+  const productAddedPage: ProductAddedPage = new ProductAddedPage();
+  const productListPage: ProductListPage = new ProductListPage();
+  const shippingStepPage: ShippingStepPage = new ShippingStepPage();
+  const signInStepPage: SignInStepPage = new SignInStepPage();
+  const summaryStepPage: SummaryStepPage = new SummaryStepPage();
+
+  it('then should be bought a t-shirt', async () => {
+    await browser.get('http://automationpractice.com/');
+    await menuContentPage.goToTShirtMenu();
+    await productListPage.selectProduct();
+    await productAddedPage.closeModal();
+    await orderSummaryPage.goToSummary();
+    await signInStepPage.sendLogin('aperdomobo@gmail.com', 'WorkshopProtractor');
+    await addressStepPage.addressStep();
+    await shippingStepPage.shippingStep();
+    await paymentStepPage.paymentStep();
+    await bankPaymentPage.bankPayment();
+    await summaryStepPage.goToSummary();
+
+    await expect(summaryStepPage.getText())
+      .toBe('Your order on My Store is complete.');
+  });
+});
