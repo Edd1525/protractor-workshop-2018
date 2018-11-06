ed6976c7aa8edc7796b3d35be10d250a3939b1a9
Edward Bustamante Acevedo <Edd1525@users.noreply.github.com>
2018-11-05 23:48:25 -0500

    add new test

---
+++ test/locators.spec.ts
@@ -0,0 +1,34 @@
+import { browser } from 'protractor';
+import { PersonalInformationPage } from '../src/page';
+
+describe('Given a form', () => {
+  const personalInformationPage: PersonalInformationPage = new PersonalInformationPage();
+
+  beforeAll(async () => {
+    await browser.get('http://toolsqa.com/automation-practice-form/');
+  });
+
+  it('When I fill all the information', async () => {
+
+    await personalInformationPage.fillForm({
+      firstName: 'Alejandro',
+      lastName: 'Perdomo',
+      sex: 'Male',
+      experience: 7,
+      profession: ['Automation Tester'],
+      tools: ['Selenium Webdriver'],
+      continent: 'South America',
+      commands: [
+        'Browser Commands',
+        'Navigation Commands',
+        'Switch Commands',
+        'Wait Commands',
+        'WebElement Commands']
+    });
+  });
+
+  it('And the title is verified', async () => {
+    await personalInformationPage.sendForm();
+    expect(personalInformationPage.getPageTitle()).toBe('Practice Automation Form');
+  });
+});
